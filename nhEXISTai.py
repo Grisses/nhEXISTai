7a11f488a7a5dcab4828aa0710a1860752ec2585
Grisses <Grissexcv@gmail.com>
2019-04-24 08:43:56 -0500

    add scraped text generator & example

--- nhEXISTai.py
+++ nhEXISTai.py
@@ -8,51 +8,57 @@ import time
 
 
 def req(pag):
-
     req_pag = requests.get('https://nhentai.net/g/'+str(pag)+'/')
 
     if req_pag.status_code == 200:
         msg = ("Magic number {0} exist".format(str(pag)))
+        print(msg)
 
     elif req_pag.status_code == 404:
         msg = ("Magic number {0} does not exist ERROR 404".format(str(pag)))
-        w_file(pag)
+        print(msg)
+        return pag
 
     else:
         msg = ("Magic number {0} does not exist UNKNOWN ERROR"
                .format(str(pag)))
-        w_file(pag)
+        print(msg)
+        return pag
 
     """THE DELAY IS FOR SAFETY! DO NOT USE VERY SHORT VISIT TIMES
     (LESS THAN 1 SECOND) TO AVOID BEING BANNED ON THE WEBSITE!"""
     time.sleep(1)
 
-    return print(msg)
-
-
-def make_file():
-    file = open('Nonexistent_magic_numbers.txt', "w")
-    file.write("Nonexistent magic numbers" + '\n')
-    file.write(time.strftime("%d/%m/%y") + " "
-               + time.strftime("%I:%M:%S") + '\n'*2)
-    file.close()
-
-
-def w_file(pag):
-    file = open('Nonexistent_magic_numbers.txt', "a")
-    file.write(str(pag) + '\n')
-    file.close()
-
 
 def main(start=1, end=270000):
-
-    make_file()
+    if start > end:
+        msg = ("The end must be greater than the end")
+        raise ValueError(msg)
+    list = []
 
     for pag in range(start, end+1):
-        req(pag)
+        Requests = req(pag)
+        if Requests is not None:
+            list.append(Requests)
 
-    print("The execution was successfully completed")
+    file_name = ('scraped ' + '(' + str(start) + "-" + str(end) + ')' + ".txt")
+    file = open(file_name, "w")
+    file.write("Nonexistent magic numbers in range: " + '(' + str(start) + "-"
+               + str(end) + ')' + '\n')
+    file.write(time.strftime("%d/%m/%y") + " "
+               + time.strftime("%I:%M:%S") + '\n'*2)
+
+    if len(list) == 0:
+        file.write("No nonexistent magic numbers in range: " + '(' + str(start)
+                   + "-" + str(end) + ')' + '\n')
+    else:
+        for pag in range(len(list)):
+            file.write(str(list[pag]) + '\n')
+
+    file.close()
+
+    return print("The execution was successfully completed")
 
 
 if __name__ == '__main__':
-    main()
+    main(165, 167)
